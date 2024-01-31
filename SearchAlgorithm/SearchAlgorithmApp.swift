671fc7cf4e4cf3b2f44f2de1464b31b49f838d8b
k-akzw <114431999+k-akzw@users.noreply.github.com>
2024-01-31 13:33:10 -0500

    Initial commit

---
+++ SearchAlgorithm/SearchAlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAlgorithmApp.swift
+//  SearchAlgorithm
+//
+//  Created by Kento Akazawa on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchAlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
